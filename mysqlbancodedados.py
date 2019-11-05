42e22c834c9433e62043b7002a4eef977268ad6d
thalianegomes <thaliane.ramos123@gmail.com>
2019-11-05 03:22:28 -0300

    feat: funções para login, buscar, editar time

--- mysqlbancodedados.py
+++ mysqlbancodedados.py
@@ -7,7 +7,7 @@ class MysqlBanco():
 		self.host = "localhost"
 		self.banco = "pooii"
 		self.user = "root"
-		self.password = "Samuel12345"
+		self.password = "silas"
 		self.conexao = pymysql.connect(host=self.host,db=self.banco,user=self.user,passwd=self.password)
 
 	def iniciar_conexao(self):
@@ -16,6 +16,7 @@ class MysqlBanco():
 			print("Conectado ao banco de dados")
 		except:
 			print("Falha ao conectar no banco de dados")
+			
 	def cadastrar_no_banco(self,siape,nome,email,senha):
 		cursor = self.conexao.cursor()
 		query = "INSERT INTO professor(siape,nome,email,senha) VALUES(%s,%s,%s,%s)"
@@ -31,14 +32,14 @@ class MysqlBanco():
 
 		return cadastrado
 
-	def cadastrarTime(self,nomeTime,membros):
+	def cadastrarTime(self,nomeTime,membros,professor):
 
 		cursor = self.conexao.cursor()
-		query = "INSERT INTO times_coders(nome_time,componentes_grupo) VALUES(%s,%s)"
+		query = "INSERT INTO times_coders(nome_time,componentes_grupo,usuario) VALUES(%s,%s,%s)"
 		cadastrado = False
 
 		try:
-			cursor.execute(query,(nomeTime,membros))
+			cursor.execute(query,(nomeTime,membros,professor))
 			MysqlBanco.count+=1
 			self.conexao.commit()
 			print("OK, Salvo no banco de dados")
@@ -50,8 +51,48 @@ class MysqlBanco():
 
 
 	def verifica_login(self,usuario,senha):
-
-		querySelect = "SELECT * FROM pro WHERE email=%s AND passwd=%s"
-		cursor.execute(querySelect,(email,passwds))
+		cursor = self.conexao.cursor()
+		querySelect = "SELECT * FROM professor WHERE email=%s AND senha=%s"
+		cursor.execute(querySelect,(usuario,senha))
 		users = cursor.fetchall()
+		times = False
+		if len(users) > 0:
+			querySelect = "SELECT * FROM times_coders WHERE usuario=%s"
+			cursor.execute(querySelect,(str(users[0][0])))
+			print(users)
+			print(str(users[0][0]))
+			times = cursor.fetchall()
+			print(times)
+			times = len(times)
+			print(times)
+			return True, list(users), times
+		else:
+			return False, list(users), times
 
+	def busca_time_editar(self,nome):
+		cursor = self.conexao.cursor()
+		querySelect = "SELECT * FROM times_coders WHERE nome_time=%s"
+		cursor.execute(querySelect,(nome))
+		time = cursor.fetchall()
+		if len(time) > 0:
+			return True,list(time)
+		else:
+			return False,list(time)
+
+
+	def editarTime(self,nome,membros,id):
+		cursor = self.conexao.cursor()
+		#cursor.execute("UPDATE carros SET nome_dono = 'Joaquim' WHERE placa = 'ABC-1234'")
+		query = "UPDATE times_coders SET nome_time=%s, componentes_grupo=%s WHERE id_time=%s"
+		cadastrado = False
+
+		try:
+			cursor.execute(query,(nome,membros,id))
+			MysqlBanco.count+=1
+			self.conexao.commit()
+			print("OK, Salvo no banco de dados")
+			cadastrado = True
+		except:
+			print("Erro ao salvar no banco de dados!!!!!")
+
+		return cadastrado
